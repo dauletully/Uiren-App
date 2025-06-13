e1c5f94572f1f3910aabf44eda5c2d7e69a98974
Nurlybakyt <begaly606@gmail.com>
2025-06-13 15:25:10 +0500

    Initial Commit

---
+++ Uiren/UirenApp.swift
@@ -0,0 +1,17 @@
+//
+//  UirenApp.swift
+//  Uiren
+//
+//  Created by Nurlybaqyt Begaly on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct UirenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
